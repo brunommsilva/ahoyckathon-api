c9ff08fd4072e8c2d95b2f0d4842dcae91ba0f84
Bruno Silva <bmmarquessilvagmail.com>
2020-06-26 15:00:04 +0100

    Transcription request log

--- src/apis/transcriptions-api.js
+++ src/apis/transcriptions-api.js
@@ -6,11 +6,13 @@ const basePath = `${__dirname}/../transcriptions/transcription-`
 exports.handle = function (request, response) {
     const file = basePath + request.params.id + '.txt';
 
+    console.log("Transcription requested: " + request.params.id)
+
     if (!fs.existsSync(file)) {
         //file exists
         response.statusCode = HttpStatus.NOT_FOUND;
         response.send({
-            "exception" : "transcription not found"
+            "exception": "transcription not found"
         });
         response.end();
     }
